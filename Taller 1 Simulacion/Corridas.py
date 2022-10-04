c5c519dc71e8d213c7d827d9e6c565860863ade6
Taavet-X <desth.x@gmail.com>
2022-10-03 22:17:46 -0500

    Update Corridas

--- Taller 1 Simulacion/Corridas.py
+++ Taller 1 Simulacion/Corridas.py
@@ -8,9 +8,9 @@ def execute(numbers, alpha):
 
     res = "*" #La cadena de texo o patron de corridas ++-++-+...
     last = "*" #El ultimo suceso o comportamiento, es * o + o -
-    counter = 1 #inicia en 1 porque siempre se cuenta algo y se incrementa (longitud de corrida actual)
+    counter = 0 #inicia en 1 porque siempre se cuenta algo y se incrementa (longitud de corrida actual)
     lastValue = numbers[0] #el ultimo numero recorrido, se inicializa como el primero del listado
-    total = 0 #Cantidad de corridas
+    total = 1 #Cantidad de corridas
     for value in numbers[1:]: #Se recorre el listado de numeros a partir de la primer posicion
         behavior = "" #Se define una variable que corresponde al comportamiento
         if value > lastValue: #en caso de que el valor actual, sea mayor al ultimo
@@ -18,7 +18,7 @@ def execute(numbers, alpha):
         elif value < lastValue: #en caso contrario
             behavior = "-" #se define como decremento
 
-        if last != behavior: #Si el ultimo comportamiento es diferente al actual,
+        if (last != behavior) and (last != "*"): #Si el ultimo comportamiento es diferente al actual,
             #quiere decir que la corrida termina alli
             try: #entonces se intenta sumar 1 al registro
                 #del diccionario que tiene la longitud de la corrida como key
@@ -32,11 +32,24 @@ def execute(numbers, alpha):
             total += 1 #Acomular una corrida en el total
         else: #En caso de que el ultimo comportamiento sea igual al actual
             counter +=1 #entonces la Longitud de corrida se incrementa en 1.
-        
+
         res += behavior #Adicionar el comportamiento a la cadena de texto
         last = behavior #define el ultimo comportamiento como el comportamiento actual
         lastValue = value #define el ultimo valor como el valor actual (valor = numero)
-
+        print(counters)
+    
+    #M
+    try: #entonces se intenta sumar 1 al registro
+        #del diccionario que tiene la longitud de la corrida como key
+        #esto arroja error, si no existe, de lo contrario lo incrementa en 1
+        counters[counter] += 1
+    except:
+        #en caso de que el error sea arrojado, entonces crea el registro
+        # con la longituda de corrida como key, y lo inicializa en 1
+        counters[counter] = 1
+    print(total)
+    #Fin M
+    
     n = len(numbers) #cantidad de numeros
     mu = (2*n - 1)/3 #media
     s = (16*n-29)/90 #varianza
